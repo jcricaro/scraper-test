c7b0b76ff4f9bcee0fad36d6f791ac4581c5ab1f
JC Ricaro <jcricaro@gmail.com>
2018-09-21 10:40:26 +0800

    Scrape job

--- app/Jobs/ScrapeSite.php
+++ app/Jobs/ScrapeSite.php
@@ -12,19 +12,26 @@ use Goutte\Client;
 class ScrapeSite implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
-    private $site;
-    private $page;
+
+    private $pages;
 
     /**
-     * Create a new job instance.
-     *
-     * @param $site
-     * @param $page
+     * @var array
      */
-    public function __construct($site, $page)
+    private $data = [];
+
+    private $page = 1;
+
+    private $nextPage = '?results=ca&meta_G=Firm&meta_D_trunc=&meta_I_trunc=';
+
+
+    /**
+     * ScrapeSite constructor.
+     * @param $pages
+     */
+    public function __construct($pages)
     {
-        $this->site = $site;
-        $this->page = $page;
+        $this->pages = $pages;
     }
 
     /**
@@ -34,13 +41,41 @@ class ScrapeSite implements ShouldQueue
      */
     public function handle()
     {
+        $crawler = $this->scrapeSite($this->pages);
+
+        return $this->data;
+    }
+
+    /**
+     * @param $pages
+     */
+    private function scrapeSite($pages)
+    {
         $client = new Client();
 
-        $crawler = $client->request('GET', 'https://www.icas.com/find-a-ca');
-        $form = $crawler->selectButton('Search');
+        $rank = $this->page * 10 - 9;
 
-        $crawler = $client->submit($form);
+        $crawler = $client->request('GET', 'https://www.icas.com/find-a-ca' . $this->nextPage . '&start_rank=' . $rank);
 
-        return $crawler;
+        $crawler->filter('b.page-item.active')->each(function($node) {
+            $this->page = $node->text();
+            $this->nextPage = $node->nextAll()->attr('href');
+        });
+
+        $crawler->filter('article > h3.title > a')->each(function($node, $index) use($client, $rank) {
+            $crawler = $client->request('GET', $node->attr('href'));
+            $name = trim($node->text());
+
+            $crawler->filter('dl > dt')->each(function($node) use ($index, $rank, $name) {
+                $this->data[$index + $rank]['name'] = $name;
+                $this->data[$index + $rank][trim(strtolower($node->text()), ": ")] = $node->nextAll()->text();
+            });
+        });
+
+        $this->page++;
+
+        if($this->page <= $pages) {
+            $this->scrapeSite($pages);
+        }
     }
 }
